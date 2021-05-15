fde68500d42bf935d4298fd2a15f75f8842855b1
dezoito <an.dezoito@gmail.com>
2021-05-15 17:16:34 -0300

    Forntend: Styled linked document selector as per design

--- src/ClientApp/src/components/DocumentSelector.jsx
+++ src/ClientApp/src/components/DocumentSelector.jsx
@@ -1,4 +1,5 @@
 import className from "classnames";
+import PropTypes from "prop-types";
 import { useStore } from "../store/global.store";
 
 const DocumentSelector = (props) => {
@@ -7,10 +8,33 @@ const DocumentSelector = (props) => {
 
   if (props.menuStyle == "links") {
     // return the selector as links
-    return (<p>Linked selector here</p>)
-
+    return (
+      <div className="linkedDocumentSelector">
+        <a
+          href="#"
+          onClick={() => setActiveTab("Invoices")}
+          className={className({
+            "linked-menu-item": true,
+            "active-linked-menu-item": activeTab === "Invoices",
+          })}
+        >
+          Invoices
+        </a>
+        &nbsp;&nbsp; | &nbsp;&nbsp;
+        <a
+          href="#"
+          onClick={() => setActiveTab("Receipts")}
+          className={className({
+            "linked-menu-item": true,
+            "active-linked-menu-item": activeTab === "Receipts",
+          })}
+        >
+          Receipts
+        </a>
+      </div>
+    );
   } else {
-    // return the selector as navbar
+    // return the selector as tabbed navbar
     return (
       <div className="documentSelector">
         <ul className="nav nav-tabs">
@@ -41,12 +65,14 @@ const DocumentSelector = (props) => {
       </div>
     );
   }
-
 };
 
+DocumentSelector.propTypes = {
+  menuStyle: PropTypes.string,
+};
 // Set default props
 DocumentSelector.defaultProps = {
-  menuStyle: "nav",
+  menuStyle: "tabs",
 };
 
 export default DocumentSelector;
